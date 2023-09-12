da965cdb9b2e87034837b1205649f28a916205b3
Joao-Kleber-Linhalis <jklinhalis@gmail.com>
2023-09-12 19:14:51 -0300

    Tecnico List

--- helpdesk-front/src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
+++ helpdesk-front/src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
@@ -1,28 +1,41 @@
-import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatTableDataSource } from '@angular/material/table';
-import { Tecnico } from 'src/app/models/tecnico';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatTableDataSource} from '@angular/material/table';
+import {Tecnico} from 'src/app/models/tecnico';
+import {TecnicoService} from 'src/app/services/tecnico.service';
 
 @Component({
-  selector: 'app-tecnico-list',
-  templateUrl: './tecnico-list.component.html',
-  styleUrls: ['./tecnico-list.component.css']
+    selector: 'app-tecnico-list',
+    templateUrl: './tecnico-list.component.html',
+    styleUrls: ['./tecnico-list.component.css']
 })
 export class TecnicoListComponent implements OnInit {
 
-  ELEMENT_DATA: Tecnico[] = [
-    {id:1,nome:'cachorro',cpf:'1234',email:'email',senha:'123',perfis : ['0'], dataCriacao : '15/08/2022'}
-  ]
+    ELEMENT_DATA: Tecnico[] = []
 
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol','acoes'];
-  dataSource = new MatTableDataSource<Tecnico>(this.ELEMENT_DATA);
-  ngOnInit(): void {
+    displayedColumns: string[] = ['position', 'name', 'weight', 'symbol', 'acoes'];
+    dataSource = new MatTableDataSource<Tecnico>(this.ELEMENT_DATA);
+    @ViewChild(MatPaginator) paginator: MatPaginator;
 
-  }
+    constructor(
+        private service: TecnicoService
+    ) {
+    }
 
-  @ViewChild(MatPaginator) paginator: MatPaginator;
+    ngOnInit(): void {
+        this.findAll();
+    }
 
-  ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator;
-  }
+    findAll() {
+        this.service.findAll().subscribe(resposta => {
+            this.ELEMENT_DATA = resposta;
+            this.dataSource = new MatTableDataSource<Tecnico>(this.ELEMENT_DATA);
+            this.dataSource.paginator = this.paginator;
+        })
+    }
+
+    applyFilter(event: Event) {
+        const filterValue = (event.target as HTMLInputElement).value;
+        this.dataSource.filter = filterValue.trim().toLowerCase();
+    }
 }
--- helpdesk-front/src/app/services/auth.service.ts
+++ helpdesk-front/src/app/services/auth.service.ts
@@ -1,16 +1,38 @@
-import { Injectable } from '@angular/core';
-import { Credenciais } from '../models/credenciais';
-import { HttpClient } from '@angular/common/http';
-import { API_CONFIG } from '../config/api.config';
+import {Injectable} from '@angular/core';
+import {Credenciais} from '../models/credenciais';
+import {HttpClient} from '@angular/common/http';
+import {API_CONFIG} from '../config/api.config';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private http: HttpClient) {}
+    jwtService: JwtHelperService = new JwtHelperService();
 
-  authenticate(creds: Credenciais) {
-    return this.http.post(`${API_CONFIG.baseUrl}/login`, creds, { observe: 'response', responseType: 'text' });
-  }
+    constructor(private http: HttpClient) {
+    }
+
+    authenticate(creds: Credenciais) {
+        return this.http.post(`${API_CONFIG.baseUrl}/login`, creds, {
+            observe: 'response', responseType: 'text'
+        });
+    }
+
+    sucessFullLogin(authToken: string){
+        localStorage.setItem('token',authToken);
+    }
+
+    isAuthentication(){
+        let token = localStorage.getItem('token');
+        if(token != null){
+            return !this.jwtService.isTokenExpired(token)
+        }
+        return false;
+    }
+
+    logout(){
+        localStorage.clear();
+    }
 }
