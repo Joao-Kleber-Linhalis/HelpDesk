2bbefd021739679be663d982b2154630526be5c9
Joao-Kleber-Linhalis <jklinhalis@gmail.com>
2023-09-16 19:18:52 -0300

    Tecnico Create

--- helpdesk-front/src/app/app.module.ts
+++ helpdesk-front/src/app/app.module.ts
@@ -33,6 +33,8 @@ import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-
 import { LoginComponent } from './components/login/login.component';
 import { ToastrModule } from 'ngx-toastr';
 import { AuthInterceptorProvider } from './interceptors/auth.interceptor';
+import { TecnicoCreateComponent } from './components/tecnico/tecnico-create/tecnico-create.component';
+import { NgxMaskModule } from 'ngx-mask';
 
 @NgModule({
   declarations: [
@@ -41,7 +43,8 @@ import { AuthInterceptorProvider } from './interceptors/auth.interceptor';
     HomeComponent,
     HeaderComponent,
     TecnicoListComponent,
-    LoginComponent
+    LoginComponent,
+    TecnicoCreateComponent
   ],
   imports: [
     BrowserModule,
@@ -71,7 +74,8 @@ import { AuthInterceptorProvider } from './interceptors/auth.interceptor';
       timeOut: 4000,
       closeButton: true,
       progressBar: true,
-    })
+    }),
+    NgxMaskModule.forRoot()
   ],
   providers: [AuthInterceptorProvider],
   bootstrap: [AppComponent]
--- helpdesk-front/src/app/components/tecnico/tecnico-create/tecnico-create.component.ts
+++ helpdesk-front/src/app/components/tecnico/tecnico-create/tecnico-create.component.ts
@@ -1,10 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Tecnico } from 'src/app/models/tecnico';
+import { TecnicoService } from 'src/app/services/tecnico.service';
 
 @Component({
   selector: 'app-tecnico-create',
   templateUrl: './tecnico-create.component.html',
   styleUrls: ['./tecnico-create.component.css']
 })
-export class TecnicoCreateComponent {
+export class TecnicoCreateComponent implements OnInit {
+
+  tecnico: Tecnico = {
+    id: "",
+    nome: "",
+    senha: "",
+    cpf: "",
+    email: "",
+    perfis: [],
+    dataCriacao: ""
+  }
+
+  nome: FormControl = new FormControl(null, Validators.minLength(3));
+  cpf: FormControl = new FormControl(null, [Validators.required, Validators.minLength(11)]);
+  email: FormControl = new FormControl(null, Validators.email);
+  senha: FormControl = new FormControl(null, Validators.minLength(3));
+
+  constructor(
+    private service: TecnicoService,
+    private toast: ToastrService,
+    private router: Router
+  ) { }
+  ngOnInit(): void {
+  }
+
+
+
+  create(): void {
+    this.service.create(this.tecnico).subscribe(resposta => {
+      this.toast.success('Técnico Cadastrado com sucesso', 'Cadastro');
+      this.router.navigate(["tecnicos"])
+    }, ex => {
+      console.log(ex);
+      if (ex.error.errors) {
+        ex.error.errors.array.forEach(element => {
+          this.toast.error(element.message);
+        })
+      } else {
+        this.toast.error(ex.error.message);
+      }
+    })
+  }
+
+  addPerfil(perfil: any): void {
+    if (this.tecnico.perfis.includes(perfil)) {
+      this.tecnico.perfis.splice(this.tecnico.perfis.indexOf(perfil), 1);
+    } else {
+      this.tecnico.perfis.push(perfil);
+    }
+  }
+
+  validarCampos(): boolean {
+    return this.nome.valid && this.cpf.valid
+      && this.email.valid && this.senha.valid;
+  }
 
 }
